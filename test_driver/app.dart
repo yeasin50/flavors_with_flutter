8e66fb70b2a7bdc4e9c1417475deba3f306f6175
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-07-29 02:06:56 +0600

    init

---
+++ test_driver/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_driver/driver_extension.dart';
+
+import 'package:flavor_with_flutter/main.dart' as app;
+
+void main() {
+  enableFlutterDriverExtension();
+
+  app.main();
+}
